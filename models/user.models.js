2f189e1d8fa0543e823e0561e64af68cd125b3c0
SubhrajitGit <subhrajitdash43@gmail.com>
2024-03-08 09:44:46 +0530

    UserSchema

---
+++ models/user.models.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    userid:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    userType:{
+        type: String,
+        required: true,
+        default: "customer",
+        enum:["customer","admin"]
+    },
+})
+
+const Student = mongoose.model("User",userSchema);
+
+module.exports = Student;
