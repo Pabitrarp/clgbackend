ad1984009788e2c7c71e908afec907492085261e
SubhrajitGit <subhrajitdash43@gmail.com>
2024-04-23 18:25:45 +0530

    Product detail

--- client/src/Pages/Productdetails.jsx
+++ client/src/Pages/Productdetails.jsx
@@ -3,11 +3,22 @@ import Layout from "../Components/Layouts/Layout";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Card from "../Components/UI/Card";
+import { useCart } from "../context/CartContext";
+useCart
 
 const Productdetails = () => {
   const params = useParams();
   const [product, setProduct] = useState({});
   const [similarProduct, setSimilarProduct] = useState([]);
+  const {cart,setCart} = useCart();
+
+//Add to Cart Function
+  const addToCart = () => {
+    const newItem = { image, name, price ,id ,quantity:1}; // Create a new item object
+    setCart([...cart, newItem]); // Add the new item to the cart
+    localStorage.setItem('cart',JSON.stringify([...cart,newItem]))
+    toast.success(`${name} added to Cart`)
+  };
 
   useEffect(() => {
     if (params?.name) {
@@ -47,30 +58,31 @@ const Productdetails = () => {
   };
   return (
     <Layout>
-      <div className="max-w-4xl py-6 mx-auto">
-        <div className="overflow-hidden bg-white rounded-lg shadow-lg">
-          <div className="flex items-center justify-center">
+      <div className="w-98 m-6">
+        <div className="  rounded-lg shadow-lg lg:flex lg:w-100 lg:h-100%">
+          <div className="flex  flex-1 flex-wrap flex-col lg:h-100% lg:border-4 bg-black-800 lg:w-100%">
             <img
               src={`http://localhost:8000/ecomm/api/v1/auth/productPhoto/${product._id}`}
               alt={product.name}
-              className="object-cover w-64 h-64"
+              className="object-cover h-3/4"
             />
-          </div>
-          <div className="p-6">
-            <h2 className="mb-4 text-2xl font-semibold">{product.name}</h2>
-            <p className="mb-4 text-gray-700">{product.description}</p>
-            <div className="flex items-center justify-between">
-              <span className="text-xl font-bold text-gray-800">
+            <div className="w-full">
+            <h2 className="p-6 text-2xl font-semibold">{product.name}</h2>
+            <p className="p-6 mb-16">{product.description}ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp</p>
+            <div className="p-6 flex justify-between">
+              <span className="text-xl font-bold ">
                 ${product.price}
               </span>
-              <button className="hover:bg-blue-600 focus:outline-none px-4 py-2 text-white bg-blue-500 rounded-md">
+              <button className="hover:bg-blue-600 focus:outline-none px-4 py-2 text-white bg-blue-500 rounded-md" onClick={addToCart}>
                 Add to Cart
               </button>
             </div>
           </div>
+          </div>
+          
         </div>
         <div>
-          <h1>Similar Products</h1>
+          <h1 className="mt-6 text-xl font-semibold">Similar Products</h1>
           {similarProduct.length < 1 ? (
             <p>No Similar Product Found </p>
           ) : (
